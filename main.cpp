fda911fe7b2e8a5e93225dc68bf0ee11ebd11a03
rabienrose <283136745@qq.com>
2016-02-12 23:34:40 -0700

    sadfasdf

--- main.cpp
+++ main.cpp
@@ -4,24 +4,19 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <map>
+#include <termios.h>
 
 #define tempFileName "pipeFile.txt"
 
 using namespace std;
-
-int chamo_execute(string appName, vector<string> args) {
-	int i;
-
-	if (appName == "") {
-		return 1;
-	}
-	return 1;
-}
-
+vector<string> historyList;
+bool isBatchMode = false;
 
 
 int execute(vector<string> args, string inputFile="", string outFile="", bool isBackstage=false){
@@ -108,7 +103,7 @@ void findFirstRepalcement(int &start, int &end, string str){
 		}else{
 			if(str[i]=='('){
 				if(findArrow == true){
-					start = i;
+					start = i+1;
 				}else{
 					findArrow= false;
 				}
@@ -140,8 +135,9 @@ string replaceStringWithFileName(string str, string fileName){
 	int start = 0;
 	int end = 0;
 	findFirstRepalcement(start, end, str);
+	//cout<<start<<":"<<end<<endl;
 	if(start != end){
-		str.replace(start-2, end- start+1, fileName);
+		str.replace(start-2, end- start+2+1, fileName);
 	}
 	return str;
 }
@@ -205,6 +201,7 @@ int execute_string_no_plancement(string &str, string outFile, string &nextInputF
 			}else{
 				nextInputFileName = _outFile;
 			}
+			break;
 		}
 	}
 	if(moreCommand){
@@ -219,7 +216,7 @@ int execute_string_no_plancement(string &str, string outFile, string &nextInputF
 		}
 	}
 	bool reBool = checkIsBackstage(args[0]);
-	cout<<_inputFile<<"|"<<_outFile<<"|"<<args[0]<<"|"<<args[1]<<endl;
+	//cout<<_inputFile<<"|"<<_outFile<<"|"<<args[0]<<"|"<<endl;
 	return execute(args, _inputFile, _outFile, reBool);
 }
 
@@ -235,15 +232,17 @@ int execute_string(string str, string outFile=""){
 		fileNameIndex++;
 		execute_string(re, fileName);
 		str = replaceStringWithFileName(str, fileName);
+		//cout <<str<<endl;
 		re = checkReplacement(str);
 	}
-	bool succAll= true;
+
+	int succAll= 1;
 	string nextInput;
 	while(str!=""){
 		string _nextInput;
 		if(execute_string_no_plancement(str, outFile, _nextInput, nextInput)!=1){
 			cout<<"one command failed!"<<endl;
-			succAll = false;
+			succAll = 0;
 			break;
 		}
 		nextInput = _nextInput;
@@ -251,34 +250,148 @@ int execute_string(string str, string outFile=""){
 	return succAll;
 }
 
+bool checkNum(char a){
+	if(a>=48 && a<=57){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+string getUsefulCom(string prefix){
+	map<string, int> data;
+	for (int i=0;i<historyList.size();i++){
+		if (historyList[i].find(prefix)!= string::npos){
+			if (data.find(historyList[i].c_str()) == data.end()){
+				data[historyList[i].c_str()] =1;
+			}else{
+				data[historyList[i].c_str()] =data[historyList[i].c_str()]+1;
+			}
+		}
+	}
+	if(data.size()==0){
+		return "";
+	}
+	int maxFreq=0;
+	string maxCom;
+	for(map<string, int>::iterator iterator = data.begin(); iterator != data.end(); iterator++) {
+	    if(iterator->second>maxFreq){
+	    	maxFreq = iterator->second;
+	    	maxCom= iterator->first;
+	    }
+	}
+	return maxCom;
+}
+
 void main_loop(void) {
 
 	int status = 1;
 	do {
 		string comLine;
-		cout << "chamo>";
+		if (!isBatchMode){
+			cout << "chamo>";
+		}
+
 		char a;
 		bool done = false;
 		do {
 			a = getchar();
+			//cout<<a;
 			if (a == EOF || a == '\n') {
 				done = true;
 				if (a == EOF) {
 					status = 0;
 				}
 			} else {
-				comLine.push_back(a);
+				if(a == '*'){
+					string reStr = getUsefulCom(comLine);
+					if (reStr!=""){
+						//for (int i=comLine.size(); i<reStr.size();i++){
+						//	comLine.push_back(reStr[i]);
+						//	cout<<reStr[i];
+						//}
+						cout <<"Best match is: "<<reStr<<endl;
+						cout<<"If you want execute this command, just hit Enter. Otherwise hit q and Enter>";
+						comLine = reStr;
+						a =getchar();
+						a =getchar();
+						done=true;
+						if(a=='q'){
+							comLine="";
+							getchar();
+						}
+					}else{
+						cout <<"No match, just hit Enter and input another command>";
+						comLine="";
+						getchar();
+						getchar();
+						done=true;
+					}
+				}else{
+					comLine.push_back(a);
+				}
+
 			}
 		} while (!done);
+		if (isBatchMode){
+			cout << comLine<<endl;
+		}
 		if (comLine == "quit" || status == 0) {
 			status = 0;
+		}else if(comLine.size() ==0 ){
+
+		}else if(comLine.size() >=2 && comLine[0] == 'f' && comLine[1] == 'c'){ //fc command
+			int state=0;//0:init, 1:read first num, 2:read sec num
+			string firstNum;
+			string secNum;
+			for(int i=0;i<comLine.size();i++){
+				if(state ==0 && comLine[i]=='-'){
+					state = 1;
+				}else if(state ==1){
+					if(comLine[i]=='-'){
+						state=2;
+					}
+					if(checkNum(comLine[i])){
+						firstNum.push_back(comLine[i]);
+					}
+				}else if(state ==2 && checkNum(comLine[i])){
+					secNum.push_back(comLine[i]);
+				}
+			}
+			//cout<<"str:"<<firstNum<<":"<<secNum<<endl;
+			if(firstNum.size()>0 && secNum.size()>0){
+				int startNum;
+				int endNum;
+				stringstream(firstNum)>>startNum;
+				stringstream(secNum)>>endNum;
+				//cout<<startNum<<":"<<endNum<<endl;
+				if(startNum<=endNum){
+					if(historyList.size()>=endNum){
+						for(int i=startNum;i<=endNum;i++){
+							cout << historyList[historyList.size()-i]<<endl;
+							status = execute_string(historyList[historyList.size()-i]);
+						}
+					}
+
+				}
+			}
 		} else {
 			status = execute_string(comLine);
+			historyList.push_back(comLine);
+
 		}
 	} while (status);
 }
 
 int main(int argc, char **argv) {
+	if(argv[1] != NULL){
+		mode_t mode = S_IRWXU;
+		int fd0 = open(argv[1], O_RDONLY, mode);
+		dup2(fd0, STDIN_FILENO);
+		close(fd0);
+		isBatchMode = true;
+	}
+
 	main_loop();
 	//vector<string> args;
 	//args.push_back("gedit");
